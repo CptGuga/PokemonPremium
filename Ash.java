41fdba8cf9f3c2534399ebd577b3d35c68c4c51b
CptGuga <cptguga@gmail.com>
2024-11-22 14:54:29 +0000

    feat: implement Ash class

---
+++ Ash.java
@@ -0,0 +1,46 @@
+import java.awt.Point;
+import java.util.HashSet;
+
+public class Ash{
+    int pokemonCounter;
+    Point currentHouse;
+    HashSet<Point> seenHouses;
+
+    public Ash(){
+        this.pokemonCounter = 1;
+        this.currentHouse = new Point(0,0);
+        this.seenHouses = new HashSet<Point>();
+        seenHouses.add(this.currentHouse);
+    }
+
+    public int getPokemonCounter(){
+        return pokemonCounter;
+    }
+
+    public void moveNorth(){
+        this.currentHouse.translate(0, 1);
+        catchPokemon();
+    }
+
+    public void moveSouth(){
+        this.currentHouse.translate(0, -1);
+        catchPokemon();
+    }
+
+    public void moveEast(){
+        this.currentHouse.translate(1, 0);
+        catchPokemon();
+    }
+
+    public void moveWest(){
+        this.currentHouse.translate(-1, 0);
+        catchPokemon();
+    }
+
+    private void catchPokemon(){
+        boolean pokemonPresent = !seenHouses.contains(currentHouse);
+        if (pokemonPresent){
+            pokemonCounter++;
+        }
+    }
+}
